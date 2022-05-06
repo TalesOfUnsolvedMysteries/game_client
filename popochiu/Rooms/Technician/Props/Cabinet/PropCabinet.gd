569e77c5037533b3b4aa164250c384bc205794ff
carenalga <mapedorr@gmail.com>
2022-05-05 22:16:24 -0500

    Created music box clues with its corresponding Documents type texture

--- popochiu/Rooms/Technician/Props/Cabinet/PropCabinet.gd
+++ popochiu/Rooms/Technician/Props/Cabinet/PropCabinet.gd
@@ -1,10 +1,20 @@
 tool
 extends Prop
 
+export(Array, String, MULTILINE) var pages = []
+export var background: Texture = null
+
 
 # ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░ métodos virtuales ░░░░
 func on_interact() -> void:
-	yield(E.run([]), 'completed')
+	yield(E.run([
+		C.walk_to_clicked(),
+		'Player: Lets see what I can find here.',
+		'...',
+		'Player: A book with notes.'
+	]), 'completed')
+	
+	G.show_documents({pages = pages, bg = background})
 
 
 func on_look() -> void:
